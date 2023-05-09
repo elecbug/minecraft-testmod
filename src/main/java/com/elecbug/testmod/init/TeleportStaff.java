9a8e60e6697733c30f61bf6f7ff32b8975bc3280
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-09 11:50:28 +0900

    staff edit

--- src/main/java/com/elecbug/testmod/init/TeleportStaff.java
+++ src/main/java/com/elecbug/testmod/init/TeleportStaff.java
@@ -1,6 +1,9 @@
 package com.elecbug.testmod.init;
 
+import com.mojang.math.Vector3d;
+
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -9,6 +12,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class TeleportStaff extends Item {
     public TeleportStaff(Properties properties) {
@@ -17,10 +21,26 @@ public class TeleportStaff extends Item {
 
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
-        BlockHitResult ray = getPlayerPOVHitResult(world, player, ClipContext.Fluid.NONE);
+        BlockHitResult ray = rayTrace(world, player, ClipContext.Fluid.NONE);
         BlockPos lookPos = ray.getBlockPos().relative(ray.getDirection());
         player.setPos(lookPos.getX(), lookPos.getY(), lookPos.getZ());
 
         return super.use(world, player, hand);
+    }
+
+    protected static BlockHitResult rayTrace(Level world, Player player, ClipContext.Fluid fluidMode) {
+        double range = 15;
+    
+        float f = player.getXRot();
+        float f1 = player.getYRot();
+        Vec3 vector3d = player.getEyePosition(1.0F);
+        float f2 = Mth.cos(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+        float f3 = Mth.sin(-f1 * ((float)Math.PI / 180F) - (float)Math.PI);
+        float f4 = -Mth.cos(-f * ((float)Math.PI / 180F));
+        float f5 = Mth.sin(-f * ((float)Math.PI / 180F));
+        float f6 = f3 * f4;
+        float f7 = f2 * f4;
+        Vec3 vector3d1 = vector3d.add((double)f6 * range, (double)f5 * range, (double)f7 * range);
+        return world.clip(new ClipContext(vector3d, vector3d1, ClipContext.Block.OUTLINE, fluidMode, player));
     }
 }
