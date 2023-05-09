4488705dcf5dffbbfc8d75fa01fc7fe527cf0b21
deveb1479@gmail.com <deveb1479@gmail.com>
2023-05-09 11:32:46 +0900

    edit

---
+++ src/main/java/com/elecbug/testmod/init/CubeEntity.java
@@ -0,0 +1,13 @@
+package com.elecbug.testmod.init;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.level.Level;
+
+public class CubeEntity extends Slime {
+
+    public CubeEntity(EntityType<? extends Slime> p_33588_, Level p_33589_) {
+        super(p_33588_, p_33589_);
+    }
+ 
+}
